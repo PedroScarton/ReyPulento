a050a84009b2a21c361985680a6130200941ee6b
Pedro Scarton <pedroscartonr@gmail.com>
2023-09-30 23:52:13 -0300

    default colors & styles

---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+  primary: "#C80BC0",
+  black: "#222222",
+  white: "#FFFFFF",
+  gray: "#D9D9D9",
+};
---
+++ src/constants/default-styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "./colors";
+
+export const defaultStyles = StyleSheet.create({
+  text: {
+    color: colors.white,
+    fontWeight: "bold",
+    fontFamily: "ConcertOne-Regular",
+    fontSize: 16,
+  },
+});

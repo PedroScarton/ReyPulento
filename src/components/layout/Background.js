ebeb97b146c8d4377cbd64479c2da8bc2e16a458
Pedro Scarton <pedroscartonr@gmail.com>
2023-09-30 23:52:04 -0300

    adding background as jpg

---
+++ src/components/layout/Background.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, ImageBackground } from "react-native";
+import BackgroundImage from "../../../assets/images/background.jpg";
+
+const Background = ({ children }) => {
+  return (
+    <ImageBackground style={styles.image} source={BackgroundImage}>
+      {children}
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Background;

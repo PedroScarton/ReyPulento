6b7aae46921d0839e76043b19b376fceb1d29a23
Pedro Scarton <pedroscartonr@gmail.com>
2023-09-30 23:51:51 -0300

    using background

--- App.js
+++ App.js
@@ -1,29 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
 import AppSplashScreen from "./AppLoading";
+import Background from "./src/components/layout/Background";
+import { defaultStyles } from "./src/constants/default-styles";
 
-import * as firebase from "firebase";
-import { firebaseConfig } from "./firebaseConfig";
+// import * as firebase from "firebase";
+// import { firebaseConfig } from "./firebaseConfig";
 
 // Inicializa Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
 
 export default function App() {
   return (
     <AppSplashScreen>
-      <Text>Rey Pulento</Text>
-      <StatusBar style="auto" />
+      <Background>
+        <Text style={defaultStyles.text}>Rey Pulento</Text>
+        <StatusBar style="light" />
+      </Background>
     </AppSplashScreen>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
